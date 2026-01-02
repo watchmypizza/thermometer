6c97d92fed07393d354a3cc06b1f0d085773666b
watchmypizza <kevinosswald@proton.me>
2026-01-02 19:25:13 +0100

    Added advanced thermal readings

--- Program.cs
+++ Program.cs
@@ -87,7 +87,7 @@ namespace thermometer.Program
 
                     existingConfig["package_manager"] = pkgM;
                     existingConfig["distribution"] = distroName;
-                    existingConfig["current_version"] = "1.4.6";
+                    existingConfig["current_version"] = "1.4.7";
                     var serializer = new SerializerBuilder()
                         .WithNamingConvention(CamelCaseNamingConvention.Instance)
                         .Build();
